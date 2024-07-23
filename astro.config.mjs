73d032f25bd36e51fd077dc90c093ba6b0758631
Blake Yeboah <blakekyeboah@gmail.com>
2024-07-23 18:19:50 +1000

    Pre live setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
+// Plugins
+import sitemap from "@astrojs/sitemap";
+import criticalCss from "astro-critical-css";
+import robotsTxt from "astro-robots-txt";
+import min from "astro-min";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.futuremerce.com',
-  integrations: [sitemap(), icon()]
+    site: "https://www.futuremerce.com",
+    integrations: [criticalCss(), sitemap(), icon(), robotsTxt(), min()],
 });
