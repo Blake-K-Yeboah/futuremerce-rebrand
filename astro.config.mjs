0a660cc6e853a2bb23f364a8f835788394079867
Blake Yeboah <blakekyeboah@gmail.com>
2024-07-24 20:41:26 +1000

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 // Plugins
 import sitemap from "@astrojs/sitemap";
-import criticalCss from "astro-critical-css";
+import criticalCSS from "astro-critical-css";
 import robotsTxt from "astro-robots-txt";
 import min from "astro-min";
 import icon from "astro-icon";
@@ -10,5 +10,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
     site: "https://www.futuremerce.com",
-    integrations: [criticalCss(), sitemap(), icon(), robotsTxt(), min()],
+    integrations: [sitemap(), icon(), robotsTxt(), min(), criticalCSS()],
 });
